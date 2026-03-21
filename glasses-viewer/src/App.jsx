cbebc341b03eb9d26f3a46221e089fb956b9e995
AlexanderTheBored <71055074+AlexanderTheBored@users.noreply.github.com>
2026-03-21 19:05:09 +0800

    addded analytics to app.jsx

--- glasses-viewer/src/App.jsx
+++ glasses-viewer/src/App.jsx
@@ -1,11 +1,13 @@
 import GlassesViewer from "./GlassesViewer";
 import AIChatbot from "./AIChatbot";
+import { Analytics } from "@vercel/analytics/react";
 
 export default function App() {
   return (
     <>
       <GlassesViewer />
       <AIChatbot />
+      <Analytics />
     </>
   );
 }
